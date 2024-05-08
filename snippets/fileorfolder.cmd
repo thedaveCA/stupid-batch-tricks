0fc2bcd9b9de72640ec9a80421ca86a22119b1de
Dave <12719274+thedaveCA@users.noreply.github.com>
2024-05-07 22:18:09 -0600

    Fix whitespace in snippets\fileorfolder.cmd

--- snippets/fileorfolder.cmd
+++ snippets/fileorfolder.cmd
@@ -3,14 +3,14 @@
 
 @echo off
 :is_directory
-    :: file_attribute can start with "d" or "-", otherwise something went wrong
-    set file_attribute=%~a1
-    if "%file_attribute:~0,1%"=="d" (
-        exit /b 0
-    ) else if "%file_attribute:~0,1%"=="-" (
-        exit /b 1
-    ) else (
-        exit /b 2
-    )
-    goto :eof
-    :: End of :is_directory
+    :: file_attribute can start with "d" or "-", otherwise something went wrong
+    set file_attribute=%~a1
+    if "%file_attribute:~0,1%"=="d" (
+        exit /b 0
+    ) else if "%file_attribute:~0,1%"=="-" (
+        exit /b 1
+    ) else (
+        exit /b 2
+    )
+    goto :eof
+    :: End of :is_directory
